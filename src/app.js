13d49a93c4e4b46b623ff4179e25479677d00e20
ElisG24 <160186837+ElisG24@users.noreply.github.com>
2024-05-31 16:01:16 +0000

    ejercicio completado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["shakira", "juanes", "j balvin", "karol G"];
+  let action = ["cantar", "pintar", "bailar", "dormir"];
+  let when = [" esta hoy", " va mañana", " viene pasadomañana", "nunca esta"];
+  let what = [" con guitarra", "el piano", "tu violin", "en acapela"];
+
+  const generateExcuse = () => {
+    let numeroAleatorioDecimal = Math.random();
+    let indiceWho = Math.floor(numeroAleatorioDecimal * who.length);
+    let indiceAction = Math.floor(numeroAleatorioDecimal * action.length);
+    let indiceWhen = Math.floor(numeroAleatorioDecimal * when.length);
+    let indiceWhat = Math.floor(numeroAleatorioDecimal * what.length);
+
+    let wordwho = who[indiceWho];
+    let wordaction = action[indiceAction];
+    let wordwhen = when[indiceWhen];
+    let wordwhat = what[indiceWhat];
+
+    let excuse = wordwho + " " + wordaction + " " + wordwhen + " " + wordwhat;
+
+    return excuse;
+  };
+
+  console.log(generateExcuse());
+
+  document.querySelector("#excuse").innerHTML = generateExcuse();
 };
